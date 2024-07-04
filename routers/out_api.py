6b3a43c12ce57f3b6914dc251eb1da66f4d95cd7
Hennico <nzanardo@fi.uba.ar>
2024-07-04 18:06:56 -0300

    Update out_api.py

--- routers/out_api.py
+++ routers/out_api.py
@@ -92,6 +92,8 @@ async def user_action_log(values: UserAction, client_db = Depends(client.get_db)
 
 @router.get("/events/metrics", summary="Retorna una entidad con los valores de las metricas.")
 async def view_matchs(client_db = Depends(client.get_db)):
+    return Response(status_code=200, content = "all good")
+
     try:
         logger.info("------ Iniciando metricas ------")
         metrics_data = {
@@ -208,6 +210,7 @@ async def view_matchs(client_db = Depends(client.get_db)):
             metrics_data["usos_de_acciones"].append(inst)
 
         #metrics_data = Metrics(**metrics_data)
-        return Response(status_code=200, content= "all good")# metrics_data #Response(status_code = 200, content = f"{metrics_data}")
+        # metrics_data #Response(status_code = 200, content = f"{metrics_data}")
+        return Response(status_code=200, content= "all good")
     except Exception as err:
         return Response(status_code = 500, content = f"An error occurred: {err}")
